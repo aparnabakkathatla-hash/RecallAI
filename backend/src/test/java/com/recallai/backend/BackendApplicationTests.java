4ccf219d77df5d95ed4687562b611ec1c9179757
Aparna <aparnabakkathatla@gmail.com>
2026-08-01 13:33:20 +0530

    Initial commit: Spring Boot backend setup

---
+++ backend/src/test/java/com/recallai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recallai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
