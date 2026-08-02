7c3a3e3d90273338f9c631b044b6140b0911ddb5
Aparna <aparnabakkathatla@gmail.com>
2026-08-02 15:38:36 +0530

    Day 3: Added User entity and UserRepository

---
+++ backend/src/main/java/com/recallai/backend/entity/User.java
@@ -0,0 +1,30 @@
+package com.recallai.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String college;
+
+    private String branch;
+
+    private Integer year;
+}
